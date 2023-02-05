e138171784cd04600498b2b70e276ba2507cb4b8
nurantezer <nurantezer68@gmail.com>
2023-02-05 16:05:12 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Middle} from "./components/Middle";
+import Footer from "./components/Footer"
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <main className="renk" style={{color:"red"}}>
+    <h3>Nuran</h3>
+
+    <Middle/>
+    <Footer/>
+  </main>
+  ) 
+
 }
 
 export default App;
---
+++ src/components/Middle.jsx
@@ -0,0 +1,7 @@
+
+
+export const Middle = () => {
+
+    return <img src="https://picsum.photos/200/300" alt="" />;
+};
+
