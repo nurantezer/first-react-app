aa8518390f50f7cf011fa583f6e36299743d39f2
nurantezer <nurantezer68@gmail.com>
2023-02-07 16:25:40 +0300

    tekrar

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'  //*gerek yok artok buna
-
-const footer = () => {
-  return (
-    <footer>Copyrigth,{new Date().getFullYear()}</footer>
-  )
-}
-
-export default footer
--- src/components/Middle.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export const Middle = () => {
-
-    return <img src="https://picsum.photos/200/300" alt="" />;
-};
-
