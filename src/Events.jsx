335e599406fa83ffd2cf09e0863937e4735f1d41
nurantezer <nurantezer68@gmail.com>
2023-02-14 15:16:01 +0300

    app

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,6 +1,3 @@
-
-
-
 const Events = () => {
 let info = 'EVENTS'
 
@@ -11,12 +8,18 @@ const handleClick = () =>{
 const handleClear = (text) =>{
 alert(text);
 } 
+
+const handleChange = (e) =>{
+    info = 'HOOKS'
+    console.log(info)
+    console.log(e.target)
+}
   return (
     <div className="container text-center mt-4">
     <div className="display-4 text-danger m-4">{info}</div>
     <button onClick={handleClick} className="btn btn-warning">CLİCK</button>
     <button className="btn btn-dark" onClick={() => handleClear('Clear Btn Pressed')}>CLEAR</button>
-
+    <button className="btn btn-danger" onClick={(e) => handleChange(e)}>CHANGE</button>
 
     </div>
   )
