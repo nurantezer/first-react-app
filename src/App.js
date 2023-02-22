7b33a34607d0d48efbacb238fa377b815e362e6c
nurantezer <nurantezer68@gmail.com>
2023-02-22 20:06:05 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import Person from "./components/Person";
-import { data } from "./data";
+
 
 function App() {
   return (
     <div>
-      {data.map((item,index) => {
-        const { name} = item;
-        return (
-          <div key={index}>
-            <h1>{name}</h1>
-            <Person  item={item} />
-          </div>
+      
         );
       
     }
--- src/components/Msg.jsx
+++
@@ -1,9 +0,0 @@
-const Msg = ({ name }) => {
-  return (
-    <div>
-      <h5>Hi, My Name is {name}</h5>
-    </div>
-  );
-};
-
-export default Msg;
