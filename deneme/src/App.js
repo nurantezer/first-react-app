9d06d1c23090ac06512691b41b2beaf81d621ba2
nurantezer <nurantezer68@gmail.com>
2023-02-27 12:17:39 +0300

    tours

--- deneme/src/App.js
+++ deneme/src/App.js
@@ -1,19 +1,8 @@
 
-import { useState } from "react";
-import { data } from "./data";
-import List from "./List";
-
 function App() {
-   const [people, setPeople] = useState(data);
+   
   return (
-    <main>
-      <section className="container">
-        <h3>{data.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
-        <button onClick={() => setPeople(data)}>Nuran</button>
-      </section>
-    </main>
+    
   );
 }
 
